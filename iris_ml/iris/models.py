8718833fa67ceefafe16c4d6d4e9cc7c1e2497fa
utkarshg1 <gaikwadujg@gmail.com>
2024-09-24 17:49:08 +0530

    migrations done

--- iris_ml/iris/models.py
+++ iris_ml/iris/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
-# Create your models here.
+
+class IrisData(models.Model):
+    sepal_length = models.FloatField(MinValueValidator(0))
+    sepal_width = models.FloatField(MinValueValidator(0))
+    petal_length = models.FloatField(MinValueValidator(0))
+    petal_width = models.FloatField(MinValueValidator(0))
+    species = models.CharField(max_length=15)
+
+    def __str__(self) -> str:
+        return self.species
